91c5feb2c2654322646d20143386a267da8b391f
caseybreen <caseyfbreen@gmail.com>
2021-12-26 16:56:36 -0800

    run all replication function

--- code/helper_functions.R
+++ code/helper_functions.R
@@ -385,3 +385,13 @@ get.bunmd.adjust.factor <- function(byear.vec = 1988:2005,
   ##     adjust.factor = e65.diff/regression.beta
   return(round(adjust.factor.2, 2))
 }
+
+
+## Source .Rmd 
+
+source_rmd = function(file, ...) {
+  tmp_file = tempfile(fileext=".R")
+  on.exit(unlink(tmp_file), add = TRUE)
+  knitr::purl(file, output=tmp_file, quiet = T)
+  source(file = tmp_file, ...)
+}
